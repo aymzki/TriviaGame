ddad7bced92f571bb068aae556d4c55576da00cd
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2019-09-07 09:51:43 -0400

    added background and styling, and wrote javascript pseudocode

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,16 @@
 $(document).ready(() => {
 
+//list of variables
+//var questions = 
+//Click the Start button to begin game
+//Show Time Remaining and Question with 3-4 choices
+//Upon clicking correct answer, Success!
+//Upon clicking wrong answer, Show correct answer
+//With no user input, it goes to the next question
+//When time remaining = 0. show correct answer, with no user input, go to next question
+//Last page-- show Correct, Incorrect, and Unanswered
+//Click Start Over button to restart 
+
+
 
 });
